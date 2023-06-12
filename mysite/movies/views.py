0b23804ada16bdf3a8a3c8ac339850c5aea51783
romajeffrey <roma.jeffrey@gmail.com>
2023-06-12 16:33:47 +0200

    show data from db

--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("Hello word")
+
 def index(request):
-    return HttpResponse("Hello word")
+    movies = Movie.objects.all()
+    output = ', '.join([m.name for m in movies])
+        # return render(request, 'index.html', {'movies': movies})
+    # return HttpResponse("Hello word")
+    return HttpResponse(output)
