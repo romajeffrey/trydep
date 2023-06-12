4c9ed8c070c6ac2cf524ea78c84148479510edcc
romajeffrey <roma.jeffrey@gmail.com>
2023-06-12 20:43:31 +0200

    before link

--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -14,3 +14,9 @@ def index(request):
     # return HttpResponse(output)
     return render(request, 'movies/index.html', {'movies': movies})
 
+
+
+def detail(request, movie_id):
+
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'detail.html', {'movie': movie})
