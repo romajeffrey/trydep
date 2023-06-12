24006954329dbb836c59f7b9c1f3bc90b333f2ab
romajeffrey <roma.jeffrey@gmail.com>
2023-06-12 16:22:47 +0200

    add movies model step

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+# new , change the display of the data in the administration
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
