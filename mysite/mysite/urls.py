aaca97054d9bac594d9f13793c0f65a94dd78800
romajeffrey <roma.jeffrey@gmail.com>
2023-06-12 16:15:52 +0200

    add project movies

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from api.models import MovieResource
+# from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movies/', include("movies.urls"))
 ]
